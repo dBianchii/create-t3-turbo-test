e2e20f4576539bc415f958bc4dbb1cf121d9796e
gdbianchii <gdbianchii@gmail.com>
2023-07-13 15:35:09 -0300

    rename acme to kdx

--- apps/expo/tailwind.config.ts
+++ apps/expo/tailwind.config.ts
@@ -1,6 +1,5 @@
+import baseConfig from "@kdx/tailwind-config";
 import { type Config } from "tailwindcss";
-
-import baseConfig from "@acme/tailwind-config";
 
 export default {
   presets: [baseConfig],
