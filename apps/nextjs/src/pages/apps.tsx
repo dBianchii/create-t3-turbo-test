50f529a29420d34aaa304278501b63f1870eaa4f
gdbianchii <gdbianchii@gmail.com>
2023-07-14 18:07:25 -0300

    try to rebuild in vercel

--- apps/nextjs/src/pages/apps.tsx
+++ apps/nextjs/src/pages/apps.tsx
@@ -1,7 +1,8 @@
-import { H1, H4, Lead } from "@ui/typography";
+import { useEffect } from "react";
+import { H1, Lead } from "@ui/typography";
+
 import KodixApp from "../components/App/KodixApp";
 import { api } from "../utils/api";
-import { useEffect } from "react";
 
 export default function Apps() {
   const { data: apps } = api.app.getInstalled.useQuery();
@@ -20,7 +21,7 @@ export default function Apps() {
             <div key={app.id}>
               <KodixApp
                 id={app.id}
-                appName={app.name}
+                appName={app.name ?? ""}
                 appDescription={app.description}
                 appUrl={app.urlApp}
                 installed={true}
