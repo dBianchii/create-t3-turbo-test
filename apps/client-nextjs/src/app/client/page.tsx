3f8df2894082797ced8d8a741d24d23ee8fc2c7d
gdbianchii <gdbianchii@gmail.com>
2023-07-16 15:34:17 -0300

    fix key

--- apps/client-nextjs/src/app/client/page.tsx
+++ apps/client-nextjs/src/app/client/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
-        {apps?.map((app) => (
-          <p>{app.name}</p>
+        {apps?.map((app, i) => (
+          <p key={i}>{app.name}</p>
         ))}
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
