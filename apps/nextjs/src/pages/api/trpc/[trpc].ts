26eaee8000c63118e4a801b3f27dc7cdc47b27c3
gdbianchii <gdbianchii@gmail.com>
2023-07-16 15:57:18 -0300

    await cors(req, res);

--- apps/nextjs/src/pages/api/trpc/[trpc].ts
+++ apps/nextjs/src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,8 @@
-import { appRouter, createTRPCContext } from "@kdx/api";
-import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { type NextApiRequest, type NextApiResponse } from "next";
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+import cors from "nextjs-cors";
+
+import { appRouter, createTRPCContext } from "@kdx/api";
 
 // export API handler
 // export default createNextApiHandler({
@@ -9,21 +11,24 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 // });
 
 // If you need to enable cors, you can do so like this:
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // Enable cors
-
-	res.setHeader('Access-Control-Allow-Credentials', '*')
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  await cors(req, res);
+  res.setHeader("Access-Control-Allow-Credentials", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
   }
   // Let the tRPC handler do its magic
   return createNextApiHandler({
