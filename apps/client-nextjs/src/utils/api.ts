3b32eaed5a79d02d0c8c49530eb3658618d053d4
gdbianchii <gdbianchii@gmail.com>
2023-07-16 17:06:30 -0300

    https://kodix.com.br

--- apps/client-nextjs/src/utils/api.ts
+++ apps/client-nextjs/src/utils/api.ts
@@ -1,12 +1,13 @@
-import type { AppRouter } from "@kdx/api";
 import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import superjson from "superjson";
 
+import type { AppRouter } from "@kdx/api";
+
 const getBaseUrl = () => {
   //if (typeof window !== "undefined") return ""; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-
+  return `https://kodix.com.br`;
   return `http://localhost:3000`; // dev SSR should use localhost
 };
 
