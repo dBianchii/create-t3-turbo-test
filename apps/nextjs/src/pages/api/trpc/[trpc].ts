9d21f196dcd628f98f791b5fc3cd5b1c1077c9fe
gdbianchii <gdbianchii@gmail.com>
2023-07-17 11:23:09 -0300

    Testing without trpc

--- apps/nextjs/src/pages/api/trpc/[trpc].ts
+++ apps/nextjs/src/pages/api/trpc/[trpc].ts
@@ -3,20 +3,11 @@ import { createNextApiHandler } from "@trpc/server/adapters/next";
 
 import { appRouter, createTRPCContext } from "@kdx/api";
 
-// export API handler
-// export default createNextApiHandler({
-//   router: appRouter,
-//   createContext: createTRPCContext,
-// });
-
-// If you need to enable cors, you can do so like this:
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  // Enable cors
-
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "*");
+  //res.setHeader("Access-Control-Allow-Origin", "*");
   // another common pattern
-  //res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT",
@@ -26,17 +17,13 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
   );
   if (req.method === "OPTIONS") {
-    return res.status(200).json({
-      body: "OK",
-    });
+    res.status(200).end();
+    return;
   }
   // Let the tRPC handler do its magic
-  const handler = createNextApiHandler({
-    router: appRouter,
-    createContext: createTRPCContext,
+  return res.status(200).json({
+    hello: "world",
   });
-
-  return handler(req, res);
 };
 
 export default handler;
