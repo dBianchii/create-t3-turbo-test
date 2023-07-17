e4a73a77d920c9d3e6a7b6ff5619c319ef1b2c83
gdbianchii <gdbianchii@gmail.com>
2023-07-17 13:30:33 -0300

    change client

--- apps/client-nextjs/src/app/client/page.tsx
+++ apps/client-nextjs/src/app/client/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
   const { data: apps } = api.app.getAll.useQuery();
   return (
     <div>
+      My insalled apps
+      <br />
       {apps?.map((app, i) => (
         <p key={i}>{app.name}</p>
       ))}
