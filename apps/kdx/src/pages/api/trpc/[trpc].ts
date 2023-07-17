2eab1f7a29a3dfd3f01182fd6d57c9db710fc332
gdbianchii <gdbianchii@gmail.com>
2023-07-17 16:10:09 -0300

    try NextCors

--- apps/kdx/src/pages/api/trpc/[trpc].ts
+++ apps/kdx/src/pages/api/trpc/[trpc].ts
@@ -1,5 +1,6 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
+import NextCors from "nextjs-cors";
 
 import { appRouter, createTRPCContext } from "@kdx/api";
 
@@ -10,25 +11,15 @@ import { appRouter, createTRPCContext } from "@kdx/api";
 // });
 
 // If you need to enable cors, you can do so like this:
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // Enable cors
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
 
-  res.setHeader("Access-Control-Allow-Credentials", "*");
-  //res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
   // Let the tRPC handler do its magic
   return createNextApiHandler({
     router: appRouter,
